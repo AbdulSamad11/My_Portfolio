a9c5e1c8513f00ed94f626fa6a341bbadd461815
AbdulSamad11 <asasamad11@gmail.com>
2021-08-23 18:42:20 +0500

    some more css set overall

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -5,12 +5,10 @@ return (
     <>
 <div className="topnav" id="myTopnav">
     <span className='logo'>Abdul Samad</span>
-  <a href="upcoming" className="active">Work</a>
+
   <a href="tools" className="active">Contact</a>
   <a href="/" className="active">Main</a>
-  <a href='null' className="icon">
-    <i className="fa fa-bars"></i>
-  </a>
+
 </div>
 </>
 );
